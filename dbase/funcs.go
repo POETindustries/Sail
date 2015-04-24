78a381a28d0de00078b3660c8a43d4410cdf5376
Andreas Raddau <raddau@poetindustries.net>
2015-04-24 13:21:20 +0200

    Completed database interface for loading pages from the database. A page is identified by its name inside the url. This has to be unique. If it is not, only the first of several pages with the same in_url value will be loaded and displayed.
    Only simple queries that rely on one parameter are supported, but not
    for long.
    
    TODO:
    Database interface needs more robustness and functionality. Next to be
    implemented should be reading writing to the db and reading multiple
    rows. 
    
    Don't keep saving important config parameters and database credentials
    in constants and variables in code. This hinders portability and makes
    recompiling necessary everytime an important parameter like passwords
    and database names are changed.

--- dbase/funcs.go
+++ dbase/funcs.go
@@ -11,6 +11,9 @@ import (
 const dbuser = "sl_user"
 const dbpass = "13381in651337"
 
+// Open establishes a connection to the database. It only asks for the
+// database's name, all other credentials should be stored in code or
+// config files
 func Open(dbname string) *sql.DB {
 	dataSource := dbuser + ":" + dbpass + "@/" + dbname
 	if db, err := sql.Open("mysql", dataSource); err == nil {
@@ -19,4 +22,17 @@ func Open(dbname string) *sql.DB {
 		println(err.Error())
 	}
 	return nil
+}
+
+// QueryRow is a wrapper about the sql function of the same name. It takes
+// a query string and the query parameters, constructs a statement out of
+// them, checks for connection problems and returns one row and an error
+// reference
+func QueryRow(query string, db *sql.DB, args string) (*sql.Row, error) {
+	if stmt, err := db.Prepare(query); err != nil {
+		println("Prepared statement creation failed: " + err.Error())
+		return nil, err
+	} else {
+		return stmt.QueryRow(args), nil
+	}
 }
