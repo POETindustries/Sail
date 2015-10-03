490885b288611c399bd0c3c6889283e6753fba6d
Andreas Raddau <raddau@poetindustries.net>
2015-10-03 21:10:56 +0200

    Made database backend explicit.

---
+++ storage/psqldb/doc.go
@@ -0,0 +1,4 @@
+// Package psqldb handles database connections and connection credentials
+// in conjunction with a PostgreSQL backend. Credentials are loaded
+// from global config.
+package psqldb
