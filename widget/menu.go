07d042a7afa14198614f63d02d5455832e6b60cd
Andreas Raddau <raddau@poetindustries.net>
2015-10-05 18:57:42 +0200

    Replaced list with nav element in widget markup.

--- widget/menu.go
+++ widget/menu.go
@@ -13,11 +13,11 @@ type Menu struct {
 // Markup returns the markup string for this widget data type. Do not
 // call this directly from template files!
 func (m *Menu) Markup(htmlTagID string) string {
-	mk := bytes.NewBufferString("<ul class='menu' id='" + htmlTagID + "'>")
+	mk := bytes.NewBufferString("<nav class='menu' id='" + htmlTagID + "'>")
 	for _, e := range m.Entries {
-		mk.WriteString("<li><a href='" + e.RefURL + "'>" + e.Name + "</a></li>")
+		mk.WriteString("<a href='" + e.RefURL + "'>" + e.Name + "</a>")
 	}
-	mk.WriteString("</ul>")
+	mk.WriteString("</nav>")
 	return mk.String()
 }
 
@@ -29,12 +29,13 @@ func (m *Menu) String() string {
 
 // MenuEntry contains all information about a specific menu entry.
 type MenuEntry struct {
-	ID      uint32
-	Name    string
-	RefID   uint32
-	RefURL  string
-	Submenu uint32
-	Pos     uint16
+	ID       uint32
+	Name     string
+	RefID    uint32
+	RefURL   string
+	Submenu  uint32
+	Pos      uint16
+	IsActive bool
 }
 
 // String prints the entry's data in an easily readable format.
