ff6645994f562902bfb88030017d57f6e8b93a4e
Andreas Raddau <raddau@poetindustries.net>
2016-03-12 10:27:24 +0100

    Responder gone, Response replaces it.

--- response/response.go
+++ response/response.go
@@ -2,7 +2,9 @@ package response
 
 import (
 	"bytes"
+	"fmt"
 	"net/http"
+	"sail/page"
 	"sail/page/data"
 )
 
@@ -26,19 +28,62 @@ func New(wr http.ResponseWriter, req *http.Request) *Response {
 		request:     req}
 }
 
-func (r *Response) Serve() {
+func (r *Response) FromCache() *Presenter {
+	if p := page.Cache().Page(r.request.URL.RequestURI()); p != nil {
+		fmt.Printf("page found in cache: %d\n", p.ID)
+		return &Presenter{page: p, markup: bytes.NewBufferString("")}
+	}
+	return r.FromURL(true)
+}
 
-	if mk := page.Cache().Markup(r.URL)); mk != nil {
-		return bytes.NewBuffer(mk)
+// NewWithID expects an id value in order to compile the
+// corresponding page data. It is guaranteed to retun a functioning
+// presenter object even if the id parameter does not lead to any data.
+func (r *Response) FromID(id uint32) *Presenter {
+	presenter := NewPresenter()
+	pages, err := page.FetchPageByID(id)
+	if len(pages) == 0 || err != nil {
+		presenter.page = page.Load404()
+	} else {
+		pages[0].Template = page.TemplateFromCache(pages[0].Template.ID)
+		presenter.page = pages[0]
+		presenter.url = pages[0].URL
+	}
+	return presenter
+}
+
+// NewWithURL expects a valid request uri in order to compile the
+// corresponding page data. It is guaranteed to retun a functioning
+// presenter object even if the url parameter does not lead to any data.
+func (r *Response) FromURL(cacheEnabled bool) *Presenter {
+	presenter := NewPresenter()
+	url := r.request.URL.RequestURI()
+	if len(url) <= 1 {
+		return r.FromID(presenter.FallbackID)
+	}
+	presenter.url = url
+	pages, err := page.FetchPageByURL(url)
+	if len(pages) == 0 || err != nil {
+		return r.FromID(presenter.FallbackID)
+	}
+	pages[0].Template = page.TemplateFromCache(pages[0].Template.ID)
+	presenter.page = pages[0]
+	if cacheEnabled {
+		page.Cache().PushPage(pages[0])
+		fmt.Printf("page added to cache: %d\n", pages[0].ID)
+	}
+	return presenter
+}
+
+func (r *Response) Serve() {
+	buf := bytes.NewBufferString(data.NOTFOUND404)
+	if markup := page.Cache().Markup(r.URL); markup != nil {
+		buf = bytes.NewBuffer(markup)
 	}
 	presenter := r.FromCache()
 	if mk, err := presenter.Compile(); err == nil {
 		page.Cache().PushMarkup(presenter.url, mk.Bytes())
-		return mk
+		buf = mk
 	}
-	return bytes.NewBufferString(data.NOTFOUND404)
-
-	var buf *bytes.Buffer
-	buf = bytes.NewBufferString(data.NOTFOUND404)
 	buf.WriteTo(r.response)
 }
