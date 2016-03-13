7035f27550e66e67716146aa9c85dabdf7f4b57c
Andreas Raddau <raddau@poetindustries.net>
2016-03-13 14:48:17 +0100

    Updated some old names in cache.

--- page/cache/cache.go
+++ page/cache/cache.go
@@ -6,7 +6,7 @@ import (
 )
 
 type cache struct {
-	pages     map[string]*content.Content
+	content   map[string]*content.Content
 	templates map[uint32]*template.Template
 	markup    map[string][]byte
 }
@@ -24,8 +24,8 @@ func (c *cache) Template(id uint32) *template.Template {
 	return c.templates[id]
 }
 
-func (c *cache) Page(url string) *content.Content {
-	return c.pages[url]
+func (c *cache) Content(url string) *content.Content {
+	return c.content[url]
 }
 
 func (c *cache) Markup(url string) []byte {
@@ -36,8 +36,8 @@ func (c *cache) PushTemplate(t *template.Template) {
 	c.templates[t.ID] = t
 }
 
-func (c *cache) PushPage(p *content.Content) {
-	c.pages[p.URL] = p
+func (c *cache) PushContent(p *content.Content) {
+	c.content[p.URL] = p
 }
 
 func (c *cache) PushMarkup(url string, m []byte) {
@@ -45,17 +45,17 @@ func (c *cache) PushMarkup(url string, m []byte) {
 }
 
 // PopTemplate removes the domain with the given id from the cache.
-// If there are cached pages that depend on the domain, they, too,
+// If there is cached content that depends on the domain, they, too,
 // are deleted from their respective caches.
 func (c *cache) PopTemplate(id uint32) {
 	var urls []string
-	for _, p := range c.pages {
+	for _, p := range c.content {
 		if p.TemplateID == id {
 			urls = append(urls, p.URL)
 		}
 	}
 	for _, url := range urls {
-		c.PopPage(url)
+		c.PopContent(url)
 		c.PopMarkup(url)
 	}
 	delete(c.templates, id)
@@ -64,8 +64,8 @@ func (c *cache) PopTemplate(id uint32) {
 // PopPage removes the given page from the cache. If there is
 // cached markup belogning to the page, it is also deleted from
 // its cache.
-func (c *cache) PopPage(url string) {
-	delete(c.pages, url)
+func (c *cache) PopContent(url string) {
+	delete(c.content, url)
 	c.PopMarkup(url)
 }
 
@@ -76,7 +76,7 @@ func (c *cache) PopMarkup(url string) {
 
 func new() *cache {
 	c := cache{}
-	c.pages = make(map[string]*content.Content)
+	c.content = make(map[string]*content.Content)
 	c.templates = make(map[uint32]*template.Template)
 	c.markup = make(map[string][]byte)
 
