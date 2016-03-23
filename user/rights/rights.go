66b82cd9e190cf5e3bb9e0221b28868bfa6bf9d8
Andreas Raddau <raddau@poetindustries.net>
2016-03-23 22:57:44 +0100

    User permission stores list of related paths.
    Every permission domain can now have url paths associated with
    it. The reasoning is that permissions control access to actions
    and to locations within the web application. The paths field
    allows checking if the currently requested url falls within a
    specific permission domain and, if so, whether the user has the
    rights to go there.

--- user/rights/rights.go
+++ user/rights/rights.go
@@ -1,5 +1,7 @@
 package rights
 
+import "errors"
+
 type Domain uint16
 type Mode uint8
 
@@ -15,19 +17,31 @@ const (
 	Delete Mode = 0x04
 )
 
+var paths = [][]string{
+	[]string{"/office/users"},
+	[]string{"/office/settings"}}
+
 // Permission is a lightweight datastructure for passing
 // rights information around between packages.
 type Permission struct {
-	id   Domain
-	mode Mode
+	id    Domain
+	mode  Mode
+	paths []string
 }
 
 func New(domain Domain, mode Mode) *Permission {
-	return &Permission{id: domain, mode: mode}
+	return &Permission{
+		id:    domain,
+		mode:  mode,
+		paths: paths[domain]}
 }
 
-func (p *Permission) Cr() bool {
+func (p *Permission) C() bool {
 	return p.mode|Create == p.mode
+}
+
+func (p *Permission) R() bool {
+	return p.mode != 0
 }
 
 func (p *Permission) U() bool {
@@ -36,4 +50,15 @@ func (p *Permission) U() bool {
 
 func (p *Permission) D() bool {
 	return p.mode|Delete == p.mode
+}
+
+func Dom(path string) (d Domain, err error) {
+	for ; d < DomainCount; d++ {
+		for _, p := range paths[d] {
+			if p == path {
+				return
+			}
+		}
+	}
+	return 0, errors.New("No domain found")
 }
