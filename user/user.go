dc6fbdebae781a59240ecd0a741dceff009248c9
Andreas Raddau <raddau@poetindustries.net>
2016-03-23 22:51:46 +0100

    User.Verify returns user object next to boolean.
    Since we are fetching from the database anyway, this presents a
    chance to use the user object right away if verification was
    successful, saving an extra fetch later when the user is actually
    needed.

--- user/user.go
+++ user/user.go
@@ -6,6 +6,7 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+// User represents a Person registered within the system.
 type User struct {
 	ID        uint32
 	Name      string
@@ -19,10 +20,13 @@ type User struct {
 	ExpDate string
 }
 
+// New returns a fresh, minimally initialized User object.
 func New() *User {
 	return &User{}
 }
 
+// ByName returns a User object from the persistent storage,
+// identified by its unique user name.
 func ByName(name string) *User {
 	us := fromStorageByName(name)
 	if len(us) < 1 {
@@ -31,16 +35,18 @@ func ByName(name string) *User {
 	return us[0]
 }
 
-// Verify returns true if user and password match entries in the
-// user database.
-func Verify(user, pass string) bool {
-	u := ByName(user)
-	if u == nil {
-		return false
+// Verify returns a User and a boolean value indicating whether user
+// and password match entries in the user database. If so, the User
+// return value is safe for use.
+//
+// Since there has to be a database request anyway, the resulting
+// User object is returned in order to give the opportunity to use it
+// right away instead of fetching anew a few lines later.
+func Verify(user, pass string) (u *User, ok bool) {
+	if u = ByName(user); u != nil {
+		ok = bcrypt.CompareHashAndPassword([]byte(u.pass), []byte(pass)) == nil
 	}
-	p := []byte(pass)
-	h := []byte(u.pass)
-	return bcrypt.CompareHashAndPassword(h, p) == nil
+	return
 }
 
 func encrypt(s string) string {
