ef0797063c03e7d87b8238eceea9a8c9b8b03fb8
Andreas Raddau <and.rad@posteo.de>
2016-03-24 15:06:59 +0100

    Added missing nil dereference guards in sessions.

--- user/session/db.go
+++ user/session/db.go
@@ -10,9 +10,7 @@ type Database struct {
 var instance *Database
 
 func new() *Database {
-	db := Database{}
-	db.sessions = make(map[string]*Session)
-	return &db
+	return &Database{sessions: make(map[string]*Session)}
 }
 
 // DB returns a pointer to the session database singleton.
@@ -28,43 +26,6 @@ func (db *Database) Add(session *Session) {
 	db.sessions[session.ID] = session
 }
 
-// Remove removes the session with the given id from the session pool.
-func (db *Database) Remove(id string) {
-	delete(db.sessions, id)
-}
-
-// ID ...is here for whatever reason.
-func (db *Database) ID(id string) string {
-	return db.sessions[id].ID
-}
-
-// User returns the user name associated with the session specified
-// by the given id.
-func (db *Database) User(id string) string {
-	return db.sessions[id].User
-}
-
-// Get returns the session specified by the given id.
-func (db *Database) Get(id string) *Session {
-	return db.sessions[id]
-}
-
-// Lang returns the session's language.
-func (db *Database) Lang(id string) string {
-	return db.sessions[id].Lang
-}
-
-// Has returns true if the session specified by the given id exists
-// in the database of active sessions.
-func (db *Database) Has(id string) bool {
-	return db.sessions[id] != nil
-}
-
-// Start resets a session's timer to the current time.
-func (db *Database) Start(id string) {
-	db.sessions[id].Start()
-}
-
 // Clean removes all expired sessions.
 func (db *Database) Clean() {
 	// TODO: expiration time is hardcoded to 6 hours.
@@ -74,4 +35,52 @@ func (db *Database) Clean() {
 			db.Remove(s.ID)
 		}
 	}
+}
+
+// Get returns the session specified by the given id.
+func (db *Database) Get(id string) *Session {
+	return db.sessions[id]
+}
+
+// Has returns true if the session specified by the given id exists
+// in the database of active sessions.
+func (db *Database) Has(id string) bool {
+	return db.sessions[id] != nil
+}
+
+// ID ...is here for whatever reason.
+func (db *Database) ID(id string) (i string) {
+	if s := db.sessions[id]; s != nil {
+		i = s.ID
+	}
+	return
+}
+
+// Lang returns the session's language.
+func (db *Database) Lang(id string) (l string) {
+	if s := db.sessions[id]; s != nil {
+		l = s.Lang
+	}
+	return
+}
+
+// Remove removes the session with the given id from the session pool.
+func (db *Database) Remove(id string) {
+	delete(db.sessions, id)
+}
+
+// Start resets a session's timer to the current time.
+func (db *Database) Start(id string) {
+	if s := db.sessions[id]; s != nil {
+		s.Start()
+	}
+}
+
+// User returns the user name associated with the session specified
+// by the given id.
+func (db *Database) User(id string) (u string) {
+	if s := db.sessions[id]; s != nil {
+		u = s.User
+	}
+	return
 }
