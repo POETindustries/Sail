5c6e944b62e8f3ca1617957a77da26b62fb34c38
Andreas Raddau <and.rad@posteo.de>
2016-04-13 02:03:00 +0200

    Create main menu item only if it's needed.

--- page/backend/presenter.go
+++ page/backend/presenter.go
@@ -63,6 +63,7 @@ func (p *Presenter) SetMessage(msg string) {
 	p.msg = msg
 }
 
+// URL returns the url currently associated with the presenter.
 func (p *Presenter) URL() string {
 	if p.url == "/office/login" && p.Session != nil {
 		return "/office/"
@@ -70,10 +71,14 @@ func (p *Presenter) URL() string {
 	return p.url
 }
 
+// SetURL should be used to change the presenter's internal
+// url after it has already been initialized.
 func (p *Presenter) SetURL(url string) {
 	p.url = url
 }
 
+// MainMenu returns available menu entry data, depending on
+// the user's access permissions.
 func (p *Presenter) MainMenu() *widget.Nav {
 	for _, e := range p.mainMenu.Entries {
 		e.Active = p.url == e.RefURL
@@ -82,29 +87,23 @@ func (p *Presenter) MainMenu() *widget.Nav {
 }
 
 func (p *Presenter) buildNav(uid uint32) *widget.Nav {
-	home := &widget.NavEntry{
-		ID: 1, Name: "Home", RefURL: "/office/"}
-	content := &widget.NavEntry{
-		ID: 2, Name: "Content", RefURL: "/office/content"}
-	user := &widget.NavEntry{
-		ID: 3, Name: "Users & Groups", RefURL: "/office/users"}
-	config := &widget.NavEntry{
-		ID: 4, Name: "Configuration", RefURL: "/office/config"}
-	maintenance := &widget.NavEntry{
-		ID: 5, Name: "Maintenance", RefURL: "/office/maintenance"}
-
-	nav := widget.Nav{Entries: []*widget.NavEntry{home}}
+	nav := widget.Nav{Entries: []*widget.NavEntry{&widget.NavEntry{
+		ID: 1, Name: "Home", RefURL: "/office/"}}}
 	if group.All().Mode(uid, rights.Content) > 1 {
-		nav.Entries = append(nav.Entries, content)
+		nav.Entries = append(nav.Entries, &widget.NavEntry{
+			ID: 2, Name: "Content", RefURL: "/office/content"})
 	}
 	if group.All().Mode(uid, rights.Users) > 1 {
-		nav.Entries = append(nav.Entries, user)
+		nav.Entries = append(nav.Entries, &widget.NavEntry{
+			ID: 3, Name: "Users & Groups", RefURL: "/office/users"})
 	}
 	if group.All().Mode(uid, rights.Config) > 1 {
-		nav.Entries = append(nav.Entries, config)
+		nav.Entries = append(nav.Entries, &widget.NavEntry{
+			ID: 4, Name: "Configuration", RefURL: "/office/config"})
 	}
 	if group.All().Mode(uid, rights.Maintenance) > 1 {
-		nav.Entries = append(nav.Entries, maintenance)
+		nav.Entries = append(nav.Entries, &widget.NavEntry{
+			ID: 5, Name: "Maintenance", RefURL: "/office/maintenance"})
 	}
 	return &nav
 }
