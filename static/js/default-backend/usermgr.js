8f07aa80172dff510e7ed83bcbdc65bea67bcbd9
Andreas Raddau <and.rad@posteo.de>
2016-04-28 01:06:17 +0200

    Added JS code for toggling between users & groups.

---
+++ static/js/default-backend/usermgr.js
@@ -0,0 +1,39 @@
+/*
+ * init setup for user management.
+ */
+(function(){
+	var toggleBtn = document.getElementById("user-group-toggle")
+	var userTable = document.getElementById("sail-backend-users-table")
+	var groupTable = document.getElementById("sail-backend-groups-table")
+	toggleBtn.onclick = function(){
+		if (toggleBtn.checked) {
+			groupTable.style.display = "table";
+			userTable.style.display = "none";
+		} else {
+			groupTable.style.display = "none";
+			userTable.style.display = "table";
+		}
+	};
+	initSelectAll(userTable)
+	initSelectAll(groupTable)
+})();
+
+/*
+ * helper function for initializing the 'select-all' functionality
+ * for the user and group tables.
+ */
+function initSelectAll(table) {
+	var boxes = table.getElementsByTagName("input")
+	boxes[0].onclick = function(){
+		for (var j = 1; j < boxes.length; j++) {
+			boxes[j].checked = boxes[0].checked;
+		}
+	};
+}
+
+/*
+ * hides all table entries that do not match the keyword.
+ */
+function filter(keyword) {
+	console.log(keyword)
+}
