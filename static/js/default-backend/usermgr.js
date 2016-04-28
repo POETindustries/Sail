f2568ade2c8ead3412d8d414cc8325405af80487
Andreas Raddau <and.rad@posteo.de>
2016-04-28 18:16:44 +0200

    Stubbed out client side code for deleting users.

--- static/js/default-backend/usermgr.js
+++ static/js/default-backend/usermgr.js
@@ -37,3 +37,24 @@ function initSelectAll(table) {
 function filter(keyword) {
 	console.log(keyword);
 }
+
+function onDeleteUsers() {
+	var userTable = document.getElementById("sail-backend-users-table");
+	console.log(selectedInTable(userTable));
+}
+
+function onDeleteGroups() {
+	var groupTable = document.getElementById("sail-backend-groups-table");
+	console.log(selectedInTable(groupTable));
+}
+
+function selectedInTable(table) {
+	var cbs = table.getElementsByTagName("tbody")[0].getElementsByTagName("input");
+	var ids = [];
+	for (var i = 0; i < cbs.length; i++) {
+		if (cbs[i].checked) {
+			ids.push(Number(cbs[i].value));
+		}
+	}
+	return ids;
+}
